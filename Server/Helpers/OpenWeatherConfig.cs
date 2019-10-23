1940114705f7639cc241879ee5518bf823dec0c9
blenz <bbluegga@yahoo.com>
2019-10-22 23:11:35 -0700

    add weather controller with weather service config

---
+++ Server/Helpers/OpenWeatherConfig.cs
@@ -0,0 +1,9 @@
+namespace WeatherApp.Helpers
+{
+    public class OpenWeatherConfig
+    {
+        public string Uri { get; set; }
+        public string Path { get; set; }
+        public string Key { get; set; }
+    }
+}
