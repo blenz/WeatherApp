09b012bdecfdc1f32a7b9a78f99678717532a80c
blenz <bbluegga@yahoo.com>
2019-10-22 23:17:17 -0700

    add weather controller with weather service config

---
+++ Server/Controllers/WeatherController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WeatherApp.Helpers;
+
+namespace WeatherApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        private readonly IOptions<OpenWeatherConfig> _openWeatherConfig;
+
+        public WeatherController(IOptions<OpenWeatherConfig> openWeatherConfig)
+        {
+            _openWeatherConfig = openWeatherConfig;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Get()
+        {
+            using(var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri(_openWeatherConfig.Value.Uri);
+
+                    var response = await client.GetAsync($"{_openWeatherConfig.Value.Path}?q=London&appid={_openWeatherConfig.Value.Key}");
+
+                    return Ok(response);
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
+                }
+            }
+        }
+    }
+}
--- Server/Startup.cs
+++ Server/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WeatherApp.Helpers;
 
 namespace WeatherApp
 {
@@ -18,6 +19,8 @@ namespace WeatherApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.Configure<OpenWeatherConfig>(Configuration.GetSection("OpenWeather"));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
